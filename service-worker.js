e803aeacaf95d8b613d80ec308defbded639d0da
Paulo Xavier <paulo.ae18@gmail.com>
2026-08-16 10:50:25 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,7 @@
-const CACHE_NAME = 'rede-apoio-v1';
+// Troque este número toda vez que publicar uma alteração no app.
+const CACHE_VERSION = 'v2';
+const CACHE_NAME = `rede-apoio-${CACHE_VERSION}`;
+
 const ASSETS = [
   './',
   './index.html',
@@ -10,19 +13,31 @@ const ASSETS = [
   './favicon.ico'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
-
